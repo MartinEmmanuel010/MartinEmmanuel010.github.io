f23bed8cd00942e1e68a5d03a4641f7c1b4cb480
MartinEmmanuel010 <156268244+MartinEmmanuel010@users.noreply.github.com>
2024-01-25 20:02:33 -0600

    Update sesion.js

--- sesion.js
+++ sesion.js
@@ -1,1 +1,25 @@
+function validateForm() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
 
+      // Validación de correo electrónico con expresión regular
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        displayErrorMessage("Correo electrónico no válido");
+        return false;
+      }
+
+      // Validación de contraseña
+      if (password.length < 8) {
+        displayErrorMessage("La contraseña debe tener más de 8 caracteres");
+        return false;
+      }
+
+      // Si todo es válido, se enviará el formulario
+      return true;
+    }
+
+    function displayErrorMessage(message) {
+      var errorMessageElement = document.getElementById('error-message');
+      errorMessageElement.innerHTML = message;
+    }
